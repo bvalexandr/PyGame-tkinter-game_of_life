60e2d5ae5e767aa7b2aaaedcfdc02c6272e52671
bvalexandr <firebird-inferno@mail.ru>
2024-11-05 17:53:17 +0300

    refactoring

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 import pygame as pg
-import random
-import copy
+
+import life_functions as lf
 
 class Game:
-    def __init__(self, width: int = 800, height: int = 600):
-        self.width: int = width
-        self.height: int = height
-        self.fps: int = 60
-        self.tile_size: int = 20
+    def __init__(self, cell_count: int = 40, cell_size: int = 20, game_speed: int = 10):
+
+        self.cell_size: int = cell_size
+        self.game_speed = game_speed
+        self.cell_count = cell_count
+        self.game_field: list[list[int]] = lf.generate_field(self.cell_count, self.cell_count)
+        
         self.clock: pg.time.Clock = pg.time.Clock()
-        self.screen: pg.Surface = pg.display.set_mode((self.width, self.height))
+        
+        self.fps: int = 60
         self.exit_game = False
         self.stop_game = True
-        self.game_speed = 10
-        self.game_field: list[list[int]] = [[0 for _ in range(self.width // self.tile_size)] for _ in range(self.height // self.tile_size)]
+
+        self.width: int = self.cell_count * self.cell_size
+        self.height: int = self.cell_count * self.cell_size
+        self.screen: pg.Surface = pg.display.set_mode((self.width, self.height))
+
         self.init_game()
 
     def init_game(self):
@@ -30,19 +36,15 @@ class Game:
                     case pg.KEYDOWN:
                         keys = pg.key.get_pressed()
                         if keys[pg.K_c]:
-                            for row in self.game_field:
-                                for col in range(len(row)):
-                                    row[col] = 0
+                            self.game_field = lf.clear_field(self.cell_count, self.cell_count)
                         if keys[pg.K_r]:
-                            for row in self.game_field:
-                                for col in range(len(row)):
-                                    row[col] = random.randint(0, 1)
+                            self.game_field = lf.randomize_field(self.cell_count, self.cell_count)
                         if keys[pg.K_SPACE]:
                             self.stop_game = not self.stop_game
                     case pg.MOUSEBUTTONDOWN:
                         pos = pg.mouse.get_pos()
-                        col = int(pos[0] // self.tile_size)
-                        row = int(pos[1] // self.tile_size)
+                        col = int(pos[0] // self.cell_size)
+                        row = int(pos[1] // self.cell_size)
                         self.game_field[row][col] = 1
             if not self.stop_game:
                 ticks += 1
@@ -54,44 +56,26 @@ class Game:
         pg.quit()
     
     def update(self):
-        copy_game_field = copy.deepcopy(self.game_field)
-        neighborts_count: int = 0
-        for row, _ in enumerate(copy_game_field):
-            for col, _ in enumerate(copy_game_field[0]):
-                neighborts_count = 0
-                for i in [-1, 0, 1]:
-                    for j in [-1, 0, 1]:
-                        if i == 0 and j == 0:
-                            continue
-                        if self.game_field[(row + i) % (self.height // self.tile_size)][(col + j) % (self.width // self.tile_size)]:
-                            neighborts_count += 1
-                cell_state = self.game_field[row][col]
-                 #Обновление ячеек
-                if cell_state and (neighborts_count == 2 or neighborts_count == 3):
-                    copy_game_field[row][col] = 1
-                elif cell_state and (neighborts_count < 2 or neighborts_count > 3):
-                    copy_game_field[row][col] = 0
-                if not cell_state and neighborts_count == 3:
-                    copy_game_field[row][col] = 1
-        self.game_field = copy.deepcopy(copy_game_field)
+        self.game_field = lf.check_field(self.game_field)
 
     def draw(self):
         pg.display.set_caption(f"Game of Life, {'play' if not self.stop_game else 'stop'}")
+
         # Заливаем задний фон
         self.screen.fill((255, 255, 255))
         
         # Рисуем сетку
         line_color: pg.Color = (100, 200, 100)
-        for row in range(self.height // self.tile_size):
-            pg.draw.line(self.screen, line_color, (0, row * self.tile_size), (self.width, row * self.tile_size))
-        for col in range(self.width // self.tile_size):
-            pg.draw.line(self.screen, line_color, (col * self.tile_size, 0), (col * self.tile_size, self.height))
+        for row in range(self.height // self.cell_size):
+            pg.draw.line(self.screen, line_color, (0, row * self.cell_size), (self.width, row * self.cell_size))
+        for col in range(self.width // self.cell_size):
+            pg.draw.line(self.screen, line_color, (col * self.cell_size, 0), (col * self.cell_size, self.height))
         
         # Рисуем ячейки
         cell_color: pg.Color = (255, 0, 0)
         for i, row in enumerate(self.game_field):
             for j, col in enumerate(row):
                 if col:
-                    pg.draw.rect(self.screen, cell_color, (j * self.tile_size, i * self.tile_size, self.tile_size, self.tile_size))
+                    pg.draw.rect(self.screen, cell_color, (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size))
 
         pg.display.update()
---
+++ life_functions.py
@@ -0,0 +1,87 @@
+import random
+import copy
+
+
+def generate_field(rows: int, cols: int) -> list[list[int]]:
+    """
+    Генерация заполненной 0 двумерной матрицы.
+
+    Arguments:
+        rows - число строк
+        cols - число столбцов
+
+    Return:
+        field - двумерная матрица
+    """
+    field: list[list[int]] = [[0 for _ in range(cols)] for _ in range(rows)]
+    return field
+
+def clear_field(rows: int, cols: int) -> list[list[int]]:
+    """
+    Очистка двумерной матрицы.
+
+    Arguments:
+        rows - число строк
+        cols - число столбцов
+
+    Return:
+        field - двумерная матрица
+    """
+    field = generate_field(rows, cols)
+    return field
+
+def randomize_field(rows: int, cols: int) -> list[list[int]]:
+    """
+    Генерация заполненной случайным образом [0..1] двумерной матрицы.
+
+    Arguments:
+        rows - число строк
+        cols - число столбцов
+
+    Return:
+        field - двумерная матрица
+    """
+    field = generate_field(rows, cols)
+    for row in range(rows):
+        for col in range(cols):
+            field[row][col] = random.randint(0, 1)
+    return field
+
+def check_field(game_field: list[list[int]]) -> list[list[int]]:
+    """
+    Проверка двумерной матрицы согласно правилам игры Game of Life.
+
+    Arguments:
+        game_field - исходная матрица
+        
+    Return:
+        field - итоговая матрица
+    """
+    copy_game_field = copy.deepcopy(game_field)
+    neighborts_count: int = 0
+
+    rows = len(game_field)
+    cols = len(game_field[0])
+
+    for row, _ in enumerate(copy_game_field):
+        for col, _ in enumerate(copy_game_field[0]):
+            # Подсчет соседей
+            # Матрица замкнута на себя
+            neighborts_count = 0
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    if i == 0 and j == 0:
+                        continue
+                    if game_field[(row + i) % rows][(col + j) % cols]:
+                        neighborts_count += 1
+            cell_state = game_field[row][col]
+
+            #Обновление ячеек
+            if cell_state and (neighborts_count == 2 or neighborts_count == 3):
+                copy_game_field[row][col] = 1
+            elif cell_state and (neighborts_count < 2 or neighborts_count > 3):
+                copy_game_field[row][col] = 0
+            if not cell_state and neighborts_count == 3:
+                copy_game_field[row][col] = 1
+
+    return copy_game_field
