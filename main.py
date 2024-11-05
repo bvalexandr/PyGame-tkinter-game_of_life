c1ed511191faaca08b76df4e8ced550902b7425b
bvalexandr <firebird-inferno@mail.ru>
2024-11-05 15:41:42 +0300

    Рисуем ячейки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 class Game:
     def __init__(self, width: int = 800, height: int = 600):
@@ -10,6 +11,7 @@ class Game:
         self.clock: pg.time.Clock = pg.time.Clock()
         self.screen: pg.Surface = pg.display.set_mode((self.width, self.height))
         self.exit_game = False
+        self.game_field: list[list[int]] = [[random.randint(0, 1) for _ in range(self.width // self.tile_size)] for _ in range(self.height // self.tile_size)]
 
     def init_game(self):
         pg.init()
@@ -31,13 +33,23 @@ class Game:
         pass
 
     def draw(self):
+        # Заливаем задний фон
         self.screen.fill((255, 255, 255))
+        
         # Рисуем сетку
         line_color: pg.Color = (100, 200, 100)
         for row in range(self.height // self.tile_size):
             pg.draw.line(self.screen, line_color, (0, row * self.tile_size), (self.width, row * self.tile_size))
         for col in range(self.width // self.tile_size):
             pg.draw.line(self.screen, line_color, (col * self.tile_size, 0), (col * self.tile_size, self.height))
+        
+        # Рисуем ячейки
+        cell_color: pg.Color = (255, 0, 0)
+        for i, row in enumerate(self.game_field):
+            for j, col in enumerate(row):
+                if col:
+                    pg.draw.rect(self.screen, cell_color, (j * self.tile_size, i * self.tile_size, self.tile_size, self.tile_size))
+
         pg.display.update()
 
 if __name__ == "__main__":
