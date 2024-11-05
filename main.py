2d57e04624b2b9351c7a3bedc0ad30b7733cfcb0
bvalexandr <firebird-inferno@mail.ru>
2024-11-05 16:56:05 +0300

    Игра готова

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import pygame as pg
 import random
+import copy
 
 class Game:
     def __init__(self, width: int = 800, height: int = 600):
-        self.init_game()
         self.width: int = width
         self.height: int = height
         self.fps: int = 60
-        self.tile_size: int = 10
+        self.tile_size: int = 20
         self.clock: pg.time.Clock = pg.time.Clock()
         self.screen: pg.Surface = pg.display.set_mode((self.width, self.height))
         self.exit_game = False
-        self.game_field: list[list[int]] = [[random.randint(0, 1) for _ in range(self.width // self.tile_size)] for _ in range(self.height // self.tile_size)]
+        self.stop_game = True
+        self.game_speed = 10
+        self.game_field: list[list[int]] = [[0 for _ in range(self.width // self.tile_size)] for _ in range(self.height // self.tile_size)]
+        self.init_game()
 
     def init_game(self):
         pg.init()
-        pg.display.set_caption("Game of Life")
     
     def run(self):
+        ticks = 0
         # Game Loop
         while not self.exit_game:
+            
             for event in pg.event.get():
                 match event.type:
                     case pg.QUIT:
@@ -34,15 +38,46 @@ class Game:
                             for row in self.game_field:
                                 for col in range(len(row)):
                                     row[col] = random.randint(0, 1)
-            self.update()
+                        if keys[pg.K_SPACE]:
+                            self.stop_game = not self.stop_game
+                    case pg.MOUSEBUTTONDOWN:
+                        pos = pg.mouse.get_pos()
+                        col = int(pos[0] // self.tile_size)
+                        row = int(pos[1] // self.tile_size)
+                        self.game_field[row][col] = 1
+            if not self.stop_game:
+                ticks += 1
+                if ticks >= self.game_speed:
+                    self.update()
+                    ticks = 0
             self.draw()
             self.clock.tick(self.fps)
         pg.quit()
     
     def update(self):
-        pass
+        copy_game_field = copy.deepcopy(self.game_field)
+        neighborts_count: int = 0
+        for row, _ in enumerate(copy_game_field):
+            for col, _ in enumerate(copy_game_field[0]):
+                neighborts_count = 0
+                for i in [-1, 0, 1]:
+                    for j in [-1, 0, 1]:
+                        if i == 0 and j == 0:
+                            continue
+                        if self.game_field[(row + i) % (self.height // self.tile_size)][(col + j) % (self.width // self.tile_size)]:
+                            neighborts_count += 1
+                cell_state = self.game_field[row][col]
+                 #Обновление ячеек
+                if cell_state and (neighborts_count == 2 or neighborts_count == 3):
+                    copy_game_field[row][col] = 1
+                elif cell_state and (neighborts_count < 2 or neighborts_count > 3):
+                    copy_game_field[row][col] = 0
+                if not cell_state and neighborts_count == 3:
+                    copy_game_field[row][col] = 1
+        self.game_field = copy.deepcopy(copy_game_field)
 
     def draw(self):
+        pg.display.set_caption(f"Game of Life, {'play' if not self.stop_game else 'stop'}")
         # Заливаем задний фон
         self.screen.fill((255, 255, 255))
         
